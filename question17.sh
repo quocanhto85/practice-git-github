55eba2adc3fa03907a8c91a886474e7a3b3423e4
Quoc Anh <quocanhto85@gmail.com>
2024-05-31 16:03:48 +0930

    17

---
+++ question17.sh
@@ -0,0 +1,20 @@
+git checkout -b branch1
+git checkout -b branch2
+git checkout main
+touch dir3/bar_copy
+git add .
+git commit -m "main"
+
+git checkout branch1
+rm -rf dir1/dir2
+touch dir1/foo
+touch newfile1
+git add .
+git commit -m "branch1"
+
+git checkout branch2
+rm dir1/dir2/foo
+touch dir1/dir2/foo_modified
+mv dir3 dir1
+git add .
+git commit -m "branch2"
